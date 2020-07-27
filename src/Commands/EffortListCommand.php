191f86b57a98ee64d76592297e092f103a98eff0
Sergey Klabukov <benjamin.yazi@gmail.com>
2020-07-27 20:41:37 +0200

    Hotfix error handle

--- src/Commands/EffortListCommand.php
+++ src/Commands/EffortListCommand.php
@@ -40,32 +40,39 @@ class EffortListCommand extends Command
     public function handle()
     {
         $msg = $this->getUpdate()->getMessage();
-        $arguments = trim(str_replace('/'.$this->name, '', $msg->text));
-        $args = explode(' ', $arguments);
-        $dt = Carbon::now();
-        // This will update the chat status to typing...
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
-        $userData = $msg->get('from');
-        $user = $this->userService->getUserOrCreate($userData->get('id'), [
-            'name' => $userData->get('first_name') . ' ' . $userData->get('last_name'),
-            'nickname' => $userData->get('username')
-        ]);
-        $from = isset($args[0])?
-            (Carbon::createFromFormat('Y-m-d', $args[0])->setTime(0,0,0)):
-            (Carbon::now()->setTime(0,0,0));
-        $to = isset($args[1])?
-            (Carbon::createFromFormat('Y-m-d', $args[1])->setTime(23,59,59)):
-            null;
-        $tasks = $this->taskService->getTasksWithEfforts($user, $from, $to);
-        $response = 'List of tasks with effort:'.PHP_EOL.PHP_EOL;
-        /** @var Task $task */
-        foreach ($tasks as $task) {
-            $response .= $this->taskService->printTaskInfo($task, $from, $to) . PHP_EOL . PHP_EOL;
+        try {
+            $arguments = trim(str_replace('/' . $this->name, '', $msg->text));
+            $args = explode(' ', $arguments);
+            // This will update the chat status to typing...
+            $this->replyWithChatAction(['action' => Actions::TYPING]);
+            $userData = $msg->get('from');
+            $user = $this->userService->getUserOrCreate($userData->get('id'), [
+                'name' => $userData->get('first_name') . ' ' . $userData->get('last_name'),
+                'nickname' => $userData->get('username')
+            ]);
+            $from = isset($args[0]) ?
+                (Carbon::createFromFormat('Y-m-d', $args[0])->setTime(0, 0, 0)) :
+                (Carbon::now()->setTime(0, 0, 0));
+            $to = isset($args[1]) ?
+                (Carbon::createFromFormat('Y-m-d', $args[1])->setTime(23, 59, 59)) :
+                null;
+            $tasks = $this->taskService->getTasksWithEfforts($user, $from, $to);
+            $response = 'List of tasks with effort:' . PHP_EOL . PHP_EOL;
+            /** @var Task $task */
+            foreach ($tasks as $task) {
+                $response .= $this->taskService->printTaskInfo($task, $from, $to) . PHP_EOL . PHP_EOL;
+            }
+            // Reply with the commands list
+            $this->replyWithMessage([
+                'text' => $response,
+                'parse_mode' => 'HTML'
+            ]);
+        } catch (\Exception $e) {
+            // Reply with the commands list
+            $this->replyWithMessage([
+                'text' => "We catch some error :^( " . $e->getMessage(),
+                'parse_mode' => 'HTML'
+            ]);
         }
-        // Reply with the commands list
-        $this->replyWithMessage([
-            'text' => $response,
-            'parse_mode' => 'HTML'
-        ]);
     }
 }
